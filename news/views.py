072a492ac6270af7ee6e23ea1f8eb74b818a8489
h4ck3r <h4ck3r@deepstate.h4ck3r>
2022-07-01 14:34:13 -0400

    adding views for function that converts day

--- news/views.py
+++ news/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt 
 
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to moringa school")
 
+def news_of_today(request):
+    date = dt.date.today()
+    html = f'''
+     <html>
+       <body>
+         <h1> {date.day}-{date.month}-{date.year}</h1>
+        </body>
+     </html
+    '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+    #function that gets the weekday number for dates
+    day_number = dt.date.weekday(dates)
+    #Making a variable of dates in form of Arrays for dates
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','sunday']
+    #Returning the actual date of the week
+    day = days[day_number]
+    return day
+
+    
