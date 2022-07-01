597bd2b1148fe5f1675678f81682ec5649a1c345
h4ck3r <h4ck3r@deepstate.h4ck3r>
2022-07-01 19:28:33 -0400

    added templates

--- tribune/settings.py
+++ tribune/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'news',
+    'bootstrap3',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
